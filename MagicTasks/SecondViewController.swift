b73e32746d3af7cccff8ab86257c6ca4d13936e7
Luiz Eduardo <luizbildzinkas@macbookpro-luiz.local>
2016-03-15 21:45:37 -0300

    Initial Commit

---
+++ MagicTasks/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MagicTasks
+//
+//  Created by Luiz Bildzinkas on 3/15/16.
+//  Copyright © 2016 Luiz Bildzinkas. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
